d0ee4dce911e604c90cb7dde90afbd7dca665aa7
ScottDotM <scottdotm@gmail.com>
2024-10-18 23:01:29 -0500

    Forgot to remove display-button from the layoutpage, was causing Vercel deployment to crash.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
